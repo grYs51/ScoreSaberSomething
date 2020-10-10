86c10f99658bcfc28573fa02a8ae12a68c73a1e3
Grace <yvesb@live.be>
2020-10-10 02:08:05 +0200

    Got navigation

--- MobiledevProject/app/src/main/java/com/example/mobiledevproject/Beatsaver/BeatsaverFragment_adapter.java
+++ MobiledevProject/app/src/main/java/com/example/mobiledevproject/Beatsaver/BeatsaverFragment_adapter.java
@@ -2,16 +2,45 @@ package com.example.mobiledevproject.Beatsaver;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.mobiledevproject.Adapters.BeatsaverMapAdapter;
+import com.example.mobiledevproject.Models.Beatsaver.beatsavermap.BeatsaverMap;
 import com.example.mobiledevproject.R;
+import com.example.mobiledevproject.profile.Fragments.profile_User_Profile;
+
+import static android.content.ContentValues.TAG;
 
 
 public class BeatsaverFragment_adapter extends Fragment {
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        if (savedInstanceState == null) {
+            getActivity().getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.beatsaver_fragment,new Beatsaver_RV())
+                    .commit();
+            Log.d(TAG, "onViewCreated: Opened beatsaver_fragment");
+        }
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.fragment_beatsaver_adapter, container, false);
+    }
 
 
 }
--- MobiledevProject/app/src/main/java/com/example/mobiledevproject/Beatsaver/Beatsaver_map_info.java
+++ MobiledevProject/app/src/main/java/com/example/mobiledevproject/Beatsaver/Beatsaver_map_info.java
@@ -2,13 +2,31 @@ package com.example.mobiledevproject.Beatsaver;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.mobiledevproject.R;
+import com.example.mobiledevproject.profile.Fragments.profile_User_Profile;
+
 public class Beatsaver_map_info extends Fragment {
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
 
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_beatsaver_info_map, container, false);
+
+        return view;
+    }
 }
