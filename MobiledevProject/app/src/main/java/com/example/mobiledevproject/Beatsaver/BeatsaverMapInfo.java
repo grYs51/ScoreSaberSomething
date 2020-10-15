6d3dd2f016dd6bcbce16167cf0f7c7bed5b67897
Grace <yvesb@live.be>
2020-10-15 17:52:21 +0200

    Added fragments and made the info activity an pageslider

--- MobiledevProject/app/src/main/java/com/example/mobiledevproject/Beatsaver/BeatsaverMapInfo.java
+++ MobiledevProject/app/src/main/java/com/example/mobiledevproject/Beatsaver/BeatsaverMapInfo.java
@@ -3,6 +3,8 @@ package com.example.mobiledevproject.Beatsaver;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.PagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import android.content.Context;
 import android.content.Intent;
@@ -20,12 +22,14 @@ import android.widget.TextView;
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.RequestBuilder;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.example.mobiledevproject.Adapters.InfoPagerAdapter;
 import com.example.mobiledevproject.Models.Beatsaver.beatsavermap.BeatsaverMap;
 import com.example.mobiledevproject.R;
+import com.google.android.material.tabs.TabItem;
+import com.google.android.material.tabs.TabLayout;
 
 public class BeatsaverMapInfo extends AppCompatActivity {
 
-    ImageView mapImage;
     TextView songName;
 
     private static final String TAG = "MapInfoActivity";
@@ -38,16 +42,25 @@ public class BeatsaverMapInfo extends AppCompatActivity {
         beatsaverMap = (BeatsaverMap) getIntent().getSerializableExtra("ree");
         Log.d(TAG, "onCreate: dataget: Title: "+ beatsaverMap.getMetaData().getSongName() );
 
-        songName = findViewById(R.id.title);
-        mapImage = findViewById(R.id.toolbar_image);
+        songName = findViewById(R.id.InfoTitle);
+        songName.setText(beatsaverMap.getMetaData().getSongName());
 
-        songName.setText(beatsaverMap.getName());
+        TabItem tabInfo = findViewById(R.id.tabInfo);
+        TabItem tabDifficulties = findViewById(R.id.tabDifficulties);
+        TabItem tabDescription = findViewById(R.id.tabDescription);
 
-        Glide.with(this)
-                .load("https://beatsaver.com"+ beatsaverMap.getCoverURL())
-                .placeholder(R.drawable.about)
-                .error(R.drawable.leaderbord)
-                .into(mapImage);
+
+        //setviewpager
+
+        ViewPager viewPager = findViewById(R.id.infoPager);
+        PagerAdapter pA = new InfoPagerAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(pA);
+        //tablayout
+
+        TabLayout tabLayout = findViewById(R.id.tablayout);
+        tabLayout.setupWithViewPager(viewPager);
+
+
 
     }
 
