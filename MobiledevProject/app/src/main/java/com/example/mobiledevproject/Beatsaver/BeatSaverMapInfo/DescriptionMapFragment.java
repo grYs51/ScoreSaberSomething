fe7b0c8886b8b3500752af1aa10350fa942ea3a6
Grace <yvesb@live.be>
2020-11-03 13:01:03 +0100

    added description

--- MobiledevProject/app/src/main/java/com/example/mobiledevproject/Beatsaver/BeatSaverMapInfo/DescriptionMapFragment.java
+++ MobiledevProject/app/src/main/java/com/example/mobiledevproject/Beatsaver/BeatSaverMapInfo/DescriptionMapFragment.java
@@ -1,21 +1,32 @@
 package com.example.mobiledevproject.Beatsaver.BeatSaverMapInfo;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.mobiledevproject.Models.Beatsaver.beatsavermap.BeatsaverMap;
 import com.example.mobiledevproject.R;
+import com.example.mobiledevproject.WebViewMap;
+
+import org.apache.commons.text.StringEscapeUtils;
+
+import static android.content.ContentValues.TAG;
 
 public class DescriptionMapFragment extends Fragment {
     BeatsaverMap beatsaverMap;
 
+    Button button;
+    TextView description;
     public DescriptionMapFragment(BeatsaverMap beatsaverMap){
         this.beatsaverMap = beatsaverMap;
     }
@@ -29,6 +40,30 @@ public class DescriptionMapFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.info_pager_fragment_description_map, container, false);
+
+        button = view.findViewById(R.id.mapButton);
+        description = view.findViewById(R.id.description);
+
+        if( beatsaverMap.getDescription() == ""){
+            description.setText("No Description given!");
+        } else{
+            description.setText(StringEscapeUtils.escapeHtml4(beatsaverMap.getDescription()));
+        }
+
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                        startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://skystudioapps.com/bs-viewer/?id="+key)));
+                String url = "https://skystudioapps.com/bs-viewer/?id="+beatsaverMap.getKey();
+                Log.d(TAG, "onClick: " + url);
+                Intent intent = new Intent(getContext(), WebViewMap.class);
+                intent.putExtra("page", url);
+                startActivity(intent);
+            }
+        });
+
+
 
         return view;
     }
